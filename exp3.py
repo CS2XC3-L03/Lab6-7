d059cf16741c05f28554910813b244bb42c762be
Saad <97638915+tariqs26@users.noreply.github.com>
2023-03-19 16:25:44 -0400

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,6 +1,10 @@
 from XC3Tree import generate_xc3_tree
 
 # print the heights of trees with degrees 0-25
+result = ''
 for i in range(26):
     new_tree = generate_xc3_tree(i)
-    print(f'degree{i:< 4} height:{new_tree.height()}')
+    result += f'degree:{i} height:{new_tree.height()}' + ', ' + ('\n' if i % 3 == 1 else '')
+
+print(result)
+
